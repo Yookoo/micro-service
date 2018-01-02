b80b452a607b8f9bfa8195aaeb18d847ab2e353f
yookoo <1006430902@qq.com>
2018-01-02 08:52:22 +0800

    zuul

---
+++ ms-spring-cloud/ms-consumer-movie-hystrix/src/main/java/com/spring/cloud/controller/MovieController.java
@@ -0,0 +1,41 @@
+package com.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.discovery.EurekaClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.spring.cloud.entity.User;
+
+@RestController
+public class MovieController {
+
+	private final static String user_service_url = "http://ms-provider-user/user/";
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	@GetMapping("/movie/user/{id}")
+	@HystrixCommand(fallbackMethod="findUserByIdCallback")
+	public User findUserById(@PathVariable Long id) {
+		return restTemplate.getForObject(user_service_url + id, User.class);
+	}
+	
+	public User findUserByIdCallback(Long id) {
+		return new User(0L);
+	}
+	
+	@GetMapping("/movie/user/eureka")
+	public String getEurekaInstance() {
+		 ServiceInstance serviceInstance = this.loadBalancerClient.choose("ms-provider-user");
+		 return serviceInstance.getHost()+":"+serviceInstance.getPort()+"-"+serviceInstance.getServiceId();
+	}
+}
